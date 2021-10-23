805d98ec2c5c4cf5a2737e96574aa024677e2390
edward <edward@softgenetics.com>
2021-10-23 00:20:16 -0500

    Add EXPORT for dll

--- src/BioebString.h
+++ src/BioebString.h
@@ -1,14 +1,15 @@
 #ifndef _BIOEBSTRING_H_
 #define _BIOEBSTRING_H_
+#include <BioebString/BioebString_EXPORT.h>
 #include <string>
 #include <unicode/unistr.h>
 #include <vector>
 
 namespace bioeb{
-  std::vector<std::string> tokenize(std::string text, char delimiter);
-  std::vector<std::string> tokenize(std::string text, std::string delimeters);
-  std::vector<icu::UnicodeString> tokenize(icu::UnicodeString text, UChar delimiter);
-  std::vector<icu::UnicodeString> tokenize(icu::UnicodeString text, icu::UnicodeString delmiters);
-  icu::UnicodeString intToStr(int number);
+  BIOEBSTRING_EXPORT std::vector<std::string> tokenize(std::string text, char delimiter);
+  BIOEBSTRING_EXPORT std::vector<std::string> tokenize(std::string text, std::string delimeters);
+  BIOEBSTRING_EXPORT std::vector<icu::UnicodeString> tokenize(icu::UnicodeString text, UChar delimiter);
+  BIOEBSTRING_EXPORT std::vector<icu::UnicodeString> tokenize(icu::UnicodeString text, icu::UnicodeString delmiters);
+  BIOEBSTRING_EXPORT icu::UnicodeString intToStr(int number);
 }
 #endif
